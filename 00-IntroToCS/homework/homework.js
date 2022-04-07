a8b33d3b1f33c3fb32a208e3ad57bfb1cfaa19da
Demetrio Jorge Frias <ic.frias.d@gmail.com>
2022-04-06 22:07:56 -0300

    Primer commit

---
+++ 00-IntroToCS/homework/homework.js
@@ -0,0 +1,18 @@
+'use strict'
+
+function BinarioADecimal(num) {
+  // tu codigo aca
+5332
+}
+
+
+function DecimalABinario(num) {
+  // tu codigo aca
+
+}
+
+
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+}
